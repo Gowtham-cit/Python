bfd3ffba56e782c024a9485cb10666828e42ec05
Gowtham-cit <52605043+Gowtham-cit@users.noreply.github.com>
2019-09-07 21:22:50 +0530

    Add files via upload

---
+++ great.py
@@ -0,0 +1,14 @@
+n=input()
+l=[]
+product=1
+for i in n:
+    a=int(i)
+    l.append(a)
+for i in l:
+    product=product*i
+if int(n)==sum(l)+product:
+    print("Great")
+else:
+    print("not")
+    
+
