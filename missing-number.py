0fe960a834541cdc7fe2ae7abe7b9dd37371aaf2
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-11-19 16:38:23 +0530

    Update missing-number.py

--- missing-number.py
+++ missing-number.py
@@ -40,9 +40,14 @@ All the numbers of nums are unique.
 #solution
 
 
-class Solution:
+class Solution1:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
         for i in range(0, n+1):
             if i not in nums:
                 return i
+       
+class Solution2:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        return sum(range(0, len(nums)+1)) - sum(nums)
