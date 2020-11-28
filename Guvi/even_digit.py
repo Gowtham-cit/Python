f814a55e9dd39ab939120f9400a3dcd0481f2642
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-11-29 00:29:32 +0530

    Update and rename even_digit.py to Guvi/even_digit.py

--- Guvi/even_digit.py
+++ Guvi/even_digit.py
@@ -1,12 +1,11 @@
-n=int(input())
-a=0
+n = int(input())
+a = 0
 for i in str(n):
-    i=int(i)
+    i = int(i)
     if i%2==0:
         a+=i
 
-if a==0:
+if a ==0:
     print("O")
 else:
     print("E")
-        
