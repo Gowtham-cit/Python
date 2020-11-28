a533dbb636c14c22ecb5eae8e51dbfee87642a2d
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-11-28 23:06:02 +0530

    Update and rename reverse-words-in-a-string-iii.py to Leet Code/reverse-words-in-a-string-iii.py

---
+++ Leet Code/reverse-words-in-a-string-iii.py
@@ -0,0 +1,11 @@
+"""
+https://leetcode.com/problems/reverse-words-in-a-string-iii/
+"""
+#solution
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.split(" ")
+        for i in range(len(s)):
+            s[i] = s[i][::-1]
+        return ' '.join(s)
