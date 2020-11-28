b20e66058471460d6f7439e95398771562358697
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-11-28 23:15:51 +0530

    Update determine_if_power_of_two.py

--- determine_if_power_of_two.py
+++ determine_if_power_of_two.py
@@ -1,37 +1,12 @@
 """
-Given an integer n, write a function to determine if it is a power of two.
-
- 
-
-Example 1:
-
-Input: n = 1
-Output: true
-Explanation: 20 = 1
-Example 2:
-
-Input: n = 16
-Output: true
-Explanation: 24 = 16
-Example 3:
-
-Input: n = 3
-Output: false
-Example 4:
-
-Input: n = 4
-Output: true
-Example 5:
-
-Input: n = 5
-Output: false
-
+https://leetcode.com/problems/power-of-two/
 """"
 #solution
 n = int(input())
 print(n>0 and bin(n).count('1') == 1)
 
 """
+Trace if n = 16
 if n = 16 then n>o becomes True  and bin(16) = 10000 
 here count of 1 will one so
 bin(16).count('1') == 1 also True
