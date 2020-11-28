10f481bc33b39b85bbeecc597f7d540a4ab14530
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-11-28 23:13:39 +0530

    Update and rename reverse-string.py to Leet Code/reverse-string.py

---
+++ Leet Code/reverse-string.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/problems/reverse-string/submissions/
+
+"""
+#solution
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        
+        """
+        
+        s.reverse()
+        return s
