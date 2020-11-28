9869fdac6fb9375df63bc25a600cee63e47cb91c
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-11-28 22:28:34 +0530

    Create jadoo-vs-koba.py

---
+++ jadoo-vs-koba.py
@@ -0,0 +1,7 @@
+"""
+https://www.hackerearth.com/practice/python/getting-started/input-and-output/practice-problems/golf/jadoo-vs-koba/description/
+"""
+
+#solution
+for i in "FGHIJKLMNOP":
+    print(ord(i))
