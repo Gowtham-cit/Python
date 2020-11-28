84557b57424261d573e583756c74752a15ae62db
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-11-28 22:13:12 +0530

    Update split-a-string-in-balanced-strings.py

--- split-a-string-in-balanced-strings.py
+++ split-a-string-in-balanced-strings.py
@@ -2,9 +2,9 @@
 https://leetcode.com/problems/split-a-string-in-balanced-strings/submissions/
 """
 
-#solution #Greedy Algorithm
+#Two solutions #Greedy Algorithm
 
-class Solution:
+class Solution1:
     def balancedStringSplit(self, s: str) -> int:
         
         c = count = 0
@@ -16,3 +16,25 @@ class Solution:
             if c == 0: count += 1
         
         return count
+    
+class Solution2:
+    def balancedStringSplit(self, s: str) -> int:
+        
+        R_count = L_count = 0
+        Balanced_count = 0
+        
+        for i in s:
+            
+            if i == 'R':
+                R_count += 1
+            else:
+                L_count += 1
+            
+            if R_count == L_count:
+                Balanced_count += 1
+                R_count = L_count = 0
+        
+        return Balanced_count
+                
+                
+        
