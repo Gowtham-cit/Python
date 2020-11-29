89c0cd33c15bc7623eb463a6d745abaf8bf2745f
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-11-29 22:55:10 +0530

    Create implement-stack-using-queues.py

---
+++ Leet Code/implement-stack-using-queues.py
@@ -0,0 +1,27 @@
+"""
+https://leetcode.com/problems/implement-stack-using-queues/
+"""
+#solution
+
+class MyStack:
+
+    def __init__(self):
+        
+        self.stack = [] #creating an empty list(stack)
+
+    def push(self, x: int) -> None:
+        
+        self.x = x
+        self.stack.append(self.x) #adding an element using uppend
+
+    def pop(self) -> int:
+        
+        return self.stack.pop()  #removing the last Element Using pop
+
+    def top(self) -> int:
+        
+        return self.stack[-1]
+
+    def empty(self) -> bool:
+        
+        return len(self.stack) == 0 #if stack is empty
