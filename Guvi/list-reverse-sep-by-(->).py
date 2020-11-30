826d42260ea37c4421b551cf5c2d014b2c78457d
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-11-30 15:49:52 +0530

    Update and rename list_reverse1.py to Guvi/list-reverse-sep-by-(->).py

---
+++ Guvi/list-reverse-sep-by-(->).py
@@ -0,0 +1,5 @@
+n = int(input())
+lst = [int(i) for i in input().split()]
+a = lst[::-1]
+print(*a, sep="->")
+
--- list_reverse1.py
+++
@@ -1,5 +0,0 @@
-n=int(input())
-lst=[int(i) for i in input().split()]
-a=lst[::-1]
-print(*a,sep="->")
-
