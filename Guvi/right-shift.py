e3d4aaf519cdb0c3462261c1e2d85d8d01eeed54
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-11-30 14:06:01 +0530

    Update and rename rightshift.py to Guvi/right-shift.py

---
+++ Guvi/right-shift.py
@@ -0,0 +1,7 @@
+def rightshift(a, b):
+    a = int(a)
+    b = int(b)
+    print(a >> b)
+a, b = input().split()
+rightshift(a, b)
+
--- rightshift.py
+++
@@ -1,7 +0,0 @@
-def rightshift(a,b):
-    a=int(a)
-    b=int(b)
-    print(a>>b)
-a,b=input().split()
-rightshift(a,b)
-
