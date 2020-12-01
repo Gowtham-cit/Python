a1fae35b366d5ebda4183334ef86e143de72dbba
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-12-02 00:09:04 +0530

    Create add-digits.py

---
+++ Leet Code/add-digits.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/problems/add-digits/
+
+"""
+#solution
+class Solution:
+    def addDigits(self, num: int) -> int:
+        
+        while num >= 10:
+            temp = 0
+            while num > 0:
+                temp += num % 10
+                num //= 10
+            num = temp
+        return num
