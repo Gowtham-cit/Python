23f9bb783546823411b8c9897703576bfc681360
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-12-04 22:31:22 +0530

    Create shift-2d-grid.py

---
+++ Leet Code/shift-2d-grid.py
@@ -0,0 +1,28 @@
+"""
+https://leetcode.com/problems/shift-2d-grid/
+"""
+#solution
+
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        
+        Row_len = len(grid)
+        Col_len = len(grid[0])
+        
+        Temp = [0] * Row_len * Col_len
+        
+        k = k % (Row_len * Col_len)
+        
+        for i in range(Row_len):
+            for j in range(Col_len):
+                
+                Temp[i * Col_len + j] = grid[i][j]
+        
+        Temp = Temp[-k:] + Temp[:-k]
+        
+        for i in range(Row_len):
+            for j in range(Col_len):
+                
+                grid[i][j] = Temp[i * Col_len + j] 
+        
+        return grid
