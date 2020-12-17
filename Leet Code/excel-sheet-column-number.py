c401a466ddfa53ec540f4306a4ef8ecb3e6ead06
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-12-17 22:47:48 +0530

    Update excel-sheet-column-number.py

--- Leet Code/excel-sheet-column-number.py
+++ Leet Code/excel-sheet-column-number.py
@@ -1,8 +1,3 @@
-"""
-https://leetcode.com/problems/excel-sheet-column-number/
-"""
-#solution
-
 class Solution:
     def titleToNumber(self, s: str) -> int:
         
@@ -25,3 +20,25 @@ class Solution:
             c += pow(26, r) * getNumber(char)
             r -= 1
         return c
+        """
+        Trace for AB
+
+        char ->A
+        c += pow(26, 1) *getNumber(A)
+        c = 26 * 1
+
+        c = 26    #c += pow(26, r) * getNumber(char)
+
+        r = 2 - 1
+
+        nxt char - > B
+
+        c += pow(26, 0) * getNumber(A)
+        c = 1 * 2
+        c = 2
+        so c = 26 + 2 #c += pow(26, r) * getNumber(char)
+             c = 28
+
+        r = 1 - 1 = 0
+
+        """
