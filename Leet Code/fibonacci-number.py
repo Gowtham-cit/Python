82531d602e54c1a01fcac737d5901dd44ea53af2
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-12-18 22:07:30 +0530

    Update and rename fibonacci-number,py to fibonacci-number.py

--- Leet Code/fibonacci-number.py
+++ Leet Code/fibonacci-number.py
@@ -5,6 +5,7 @@ https://leetcode.com/problems/fibonacci-number
 class Solution:
     def fib(self, n: int) -> int:
         
+        
         if n < 2:
             return n
         return self.fib(n-1) + self.fib(n-2)
