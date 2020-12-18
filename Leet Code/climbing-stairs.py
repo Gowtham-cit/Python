fbefd2ab76614eb71763ab557df47099f888e2f1
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2020-12-18 22:21:00 +0530

    Create climbing-stairs.py

---
+++ Leet Code/climbing-stairs.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/problems/climbing-stairs/
+"""
+#solution
+#like fibonacci series
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        if n == 1:
+            return 1
+        a, b = 1, 2
+        for i in range(2, n):
+            tmp = b
+            b = a+b
+            a = tmp
+        return b
