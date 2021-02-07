38c9322c4c19402a6d155e45677cd1b3233d50a4
Gowtham Ramesh <52605043+Gowtham-cit@users.noreply.github.com>
2021-02-07 22:42:17 +0530

    Update BubbleSort.py

--- Leet Code/BubbleSort.py
+++ Leet Code/BubbleSort.py
@@ -2,16 +2,16 @@ def BubbleSort(lst):
 
         n = len(lst)
         for i in range(n):
+                
                 for j in range(0,  n - i - 1):
+                        
                         if lst[j] > lst[j+1]:
                                 lst[j], lst[j+1] = lst[j+1], lst[j]
                         
-                        #print(lst)  step by step
-                #print(lst) Each iteration last step
+                        
+                
         print(lst) #final sortted array
 #driver code        
 lst =[int(x) for x in input().split()]
 
 BubbleSort(lst)
-                        
-        
